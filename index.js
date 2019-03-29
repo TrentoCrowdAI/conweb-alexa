8af78e8770d24eb81916dbaad43d3d8ae63c7982
pietro <pietrochitto@hotmail.it>
2019-03-29 11:11:51 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+
 const PORT = process.env.PORT || 443
 
-express()
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+app
+    .get('/', (req, res) => res.send(JSON.parse(req)))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
