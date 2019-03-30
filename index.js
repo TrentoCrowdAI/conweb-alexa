8d03e8b60c8aefa717ed22b9b2f187d54c73c63b
pietro <pietrochitto@hotmail.it>
2019-03-30 16:02:15 +0100

    prove

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
 //app.use(bodyParser.json()); // for parsing application/json
 //app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.get('/', function (req, res, next){
     res.render('index', {request: req});
-  console.log(`request on get: ${ req.requestId }`);
+  console.log(`request on get: ${ req.body }`);
 });
 
 app.post('/', function (req, res, next) {
   res.render('index', {request: req.body});
-  console.log(`request on post: ${ req }`);
+  console.log(`request on post: ${ req.body.request }`);
 }); 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
