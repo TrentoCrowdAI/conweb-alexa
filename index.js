e9d32505ddd903633b477f9bdaed33342b4be0c8
pietro <pietrochitto@hotmail.it>
2019-03-31 17:23:50 +0200

    prove

--- index.js
+++ index.js
@@ -3,22 +3,13 @@ const PORT = process.env.PORT || 443 ;
 var createError = require('http-errors');
 var path = require('path');
 var logger = require('morgan');
+const Alexa = require('ask-sdk-core');
+
+const skillID ="amzn1.ask.skill.8e874ee7-ad2f-4b42-90ec-5e9490c965e3";
 
 var app = express();
-//var upload = multer(); // for parsing multipart/form-data
-
-var response= {
-    "version": "1.0",
-
-    "response": {
-      "outputSpeech": {
-        "type": "PlainText",
-        "text": "Hello world",
-        "playBehavior": "REPLACE_ENQUEUED"      
-      },      
-      "shouldEndSession": true
-    }
-  }
+//var upload = multer(); 
+// for parsing multipart/form-data
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,15 +18,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
 
-//app.use(bodyParser.json()); // for parsing application/json
-//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.get('/', function (req, res, next){
     res.render('index', {request: req});
   console.log(`request on get: ${ req.body }`);
 });
 
 app.post('/', function (req, res, next) {
-    console.log(`request on post: ${JSON.stringify(req)}`)
+    console.log(`request on post: ${ req.body.session.type.application.applicationId }`);
+    clientId = req.body.session.type.application.applicationId;
     //console.log(`request on post: ${ req.body.request.type }`);
     //console.log(`request on post: ${ req.body.request.timestamp }`);
     //console.log(`request on post: ${ req.body.request.requestId }`);
@@ -43,20 +35,10 @@ app.post('/', function (req, res, next) {
     //console.log(`request on post: ${ req.body.request.reason }`);
     //console.log(`request on post: ${ req.body.request.error.type }`);
     //console.log(`request on post: ${ req.body.request.error.message }`);
-    res.json({
-        "version": "1.0",
     
-        "response": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Hello world",
-            "playBehavior": "REPLACE_ENQUEUED"      
-          },      
-          "shouldEndSession": true
-        }
-      }
-      
-      )
+    
+    
+    res.json();
     console.log('response sended')
 }); 
 
